24b3cede0f7d82880618f2a9c419945511d3ce8e
hugosgitlab <hugo.marques@unbabel.com>
2020-07-16 12:51:18 +0100

    🌈 initial code

---
+++ booking_class.js
@@ -0,0 +1,89 @@
+const puppeteer = require('puppeteer');
+const fs = require("fs");
+require('dotenv').config();
+
+function string_date(next_days = 0){
+  let today = new Date();
+  today.setDate(today.getDate() + next_days);
+
+  const d = String(today.getDate());
+  const m = String(today.getMonth());
+  const yyyy = today.getFullYear();
+
+  return (yyyy + '-' + m + '-' + d);
+}
+
+const automation = async (crossfit_class_local, crossfit_class_hour, days_in_advance_registration) => {
+  const crossfit_class_date = string_date(days_in_advance_registration);
+
+  const browser = await puppeteer.launch({headless: true});
+  const page = await browser.newPage();
+
+  try {
+    await page.goto(process.env.REGIBOX_URL);
+    // the page clock is some seconds delayed
+    await page.waitFor(3500);
+
+    // search for the box
+    await page.waitFor('input[type=search]');
+    await page.type('input[type=search]', process.env.BOX_NAME);
+    await page.waitFor(250);
+    await page.click('li[class="item-content"]');
+
+    // login
+    await page.waitFor('div.page-current > div[class~="login-screen-content"]');
+    await page.type('input[name=login]', process.env.LOGIN);
+    await page.type('input[name=password]', process.env.PASSWORD);
+    await page.click('input[value=LOGIN]');
+
+    // wait for slapsh screen to disappear
+    await page.waitFor('div[id="splash_screen"]', {visible: false});
+
+    // enter registe class page
+    const registe_class_selector = "#feed_minhas_aulasxxx > div > div > div.card-footer > a:nth-child(1)";
+    await page.waitForSelector(registe_class_selector, {visible: true});
+    await page.click(registe_class_selector);
+
+    // wait for the registe class page to load
+    await page.waitForSelector('div[class~=router-transition-forward]');
+    await page.waitForSelector('div[class~=router-transition-forward]', {hidden: true});
+
+    // select the day to schedule
+    const crossfit_class_selector = `div[data-date="${crossfit_class_date}"]`;
+    await page.click(crossfit_class_selector);
+    await page.waitFor(`${crossfit_class_selector}[class~="calendar-day-selected"]`);
+
+    // register the class hour
+
+    const register_button_xpath = `//*[contains(text(), '${crossfit_class_local}')]/../../div[3]/div[contains(text(), '${crossfit_class_hour}')]/../div[3]/button`;
+    const register_button = await page.$x(register_button_xpath);
+    if (register_button.length == 0) {
+      // scroll down to see the missing button
+      await page.evaluate(() => {
+        let elem = document.querySelector("#feed_cal");
+        elem.scrollTop = elem.scrollHeight;
+      });
+      throw `Register button for class at '${crossfit_class_hour}' not found`;
+    }
+
+    await register_button[0].click();
+    await page.waitFor('button[class="col button button-small button-fill color-red"]', {visible: true});
+    await browser.close();
+  }
+  catch (e) {
+    const error_date = + new Date;
+    const error_dir = process.env.ERROR_DIR
+    // const error_html = await page.content();
+
+    if (!fs.existsSync(error_dir)) fs.mkdirSync(error_dir);
+    // fs.writeFileSync(`${error_dir}/${error_date}_error_dump.txt`, `${e} ${e.stack}`);
+    // fs.writeFileSync(`${error_dir}/${error_date}_html_dump.html`, error_html);
+
+    await page.screenshot({path: `${error_dir}/${error_date}_screenshot.png`});
+
+    await browser.close();
+    throw e
+  }
+}
+
+exports.automation = automation;
---
+++ index.js
@@ -0,0 +1,32 @@
+
+require('dotenv').config();
+const booking_class = require('./booking_class');
+const express = require('express');
+const Agenda = require('agenda');
+const Agendash = require('agendash');
+
+const app = express();
+const agenda = new Agenda({db: {address: process.env.MONGO_URL}});
+
+const appTitle = "CrossFit Scheduler";
+const port = parseInt(process.env.PORT)
+
+const defaultCrossfitClassCron = process.env.DEFAULT_CROSSFIT_CLASS_CRON;
+const defaultCrossfitClassReservation = {
+  local: process.env.DEFAULT_CROSSFIT_CLASS_LOCAL,
+  hour: process.env.DEFAULT_CROSSFIT_CLASS_HOUR,
+  days_in_advance: parseInt(process.env.DEFAULT_DAYS_IN_ADVANCE_REGISTRATION)
+}
+agenda.define('CrossFit Class', async job => {
+	const {local, hour, days_in_advance} = job.attrs.data;
+  await booking_class.automation(local, hour, days_in_advance);
+});
+
+(async function() {
+  await agenda.start();
+  agenda.every(defaultCrossfitClassCron, 'CrossFit Class', defaultCrossfitClassReservation);
+})();
+
+app.use('/', Agendash(agenda, {title: appTitle}));
+
+app.listen(port, () => console.log(`${appTitle} listening at http://localhost:${port}`))
