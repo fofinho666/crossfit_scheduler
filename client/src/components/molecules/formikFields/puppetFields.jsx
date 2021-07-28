ff04ed8f7f922f2600cfbafdd3d539fc78261d7d
fofinho666 <a_tralha_do_hugo@hotmail.com>
2021-07-28 14:32:15 +0100

    Add default value if no puppet does not have fields

--- client/src/components/molecules/formikFields/puppetFields.jsx
+++ client/src/components/molecules/formikFields/puppetFields.jsx
@@ -6,12 +6,12 @@ import TextField from "./textField";
 const PuppetFields = ({ puppet }) => {
   const { setFieldValue } = useFormikContext()
 
-  useEffect(() => { 
-    if (puppet){
+  useEffect(() => {
+    if (puppet) {
       setFieldValue("puppet-form-puppet", puppet.puppet)
-    } 
+    }
   }, [])
-  
+
   const validateField = (value) => {
     if (!value || value.trim() === "") {
       return `You must provide a value`
@@ -23,9 +23,15 @@ const PuppetFields = ({ puppet }) => {
   return <div className="columns is-mobile is-multiline is-centered">
     <For each="field" index="idx" of={puppet.fields}>
       <div className="column is-narrow" key={`field-${idx}`} >
-        <TextField name={`puppet-form-${field}`} label={sentenceCase(field)}  validate={validateField}/>
+        <TextField name={`puppet-form-${field}`} label={sentenceCase(field)} validate={validateField} />
       </div>
     </For>
+    
+    <If condition={puppet.fields.length == 0}>
+      <div className="column is-narrow pb-5" >
+        <p>No values needed</p>
+      </div>
+    </If>
   </div>
 }
 
